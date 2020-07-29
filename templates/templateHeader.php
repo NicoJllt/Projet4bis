5887a5d2efd58d820531e2229c609e5ebe135a88
Juillet Nicolas <juillet.n@hotmail.fr>
2020-07-29 14:09:08 +0200

    modif namespace 1

---
+++ templates/templateHeader.php
@@ -0,0 +1,27 @@
+<header>
+    <section class="row">
+        <div class="col-lg-12">
+            <nav class="summary">
+                <ul>
+                    <li><a href="index.php?action=synopsis" class="synopsis-nav">Synopsis</a></li>
+                    <li><a href="index.php" class="home-nav">Accueil</a></li>
+                    <li><a href="index.php?action=showLastNews" class="last-episodes-nav">Derniers épisodes</a></li>
+
+                    <li><input type="search" class="site-search-nav" name="q" aria-label="Search"></li>
+                    <li><a href="index.php?action=subscribe-page" class="register-nav">S'inscrire</a></li>
+                    <li><a href="index.php?action=login-page" class="connection-nav">Se connecter</a></li>
+                    <li><a href="index.php?action=logout" class="logout-nav">Se déconnecter</a></li>
+                </ul>
+            </nav>
+        </div>
+    </section>
+</header>
+
+<section class="row">
+    <div class="col-lg-12">
+        <div class="title-framed">
+            <h1 class="book-title">Billet simple pour l'Alaska</h1>
+            <h2 class="book-author">Par Jean Forteroche</h2>
+        </div>
+    </div>
+</section>
