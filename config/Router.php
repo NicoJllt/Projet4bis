12d4809f4a680e33c231a96ae4b17157f7a96272
Juillet Nicolas <juillet.n@hotmail.fr>
2020-08-16 19:57:27 +0200

    Controller

--- config/Router.php
+++ config/Router.php
@@ -1,31 +1,38 @@
 <?php
 
 namespace App\config;
+
 use App\src\controller\FrontController;
+use App\src\controller\ErrorController;
+
 use Exception;
 
 class Router
 {
+    private $frontController;
+    private $errorController;
+
+    public function __construct()
+    {
+        $this->frontController = new FrontController();
+        $this->errorController = new ErrorController();
+    }
+
     public function run()
     {
-        try{
-            if(isset($_GET['route']))
-            {
-                if($_GET['route'] === 'episode'){
+        try {
+            if (isset($_GET['route'])) {
+                if ($_GET['route'] === 'episode') {
+                    $this->frontController->episode($_GET['episodeId']);
                     require '../templates/singleEpisode.php';
+                } else {
+                    $this->errorController->errorNotFound();
                 }
-                else{
-                    echo 'page inconnue';
-                }
+            } else {
+                $this->frontController->home();
             }
-            else{
-                $frontController = new FrontController();
-                $frontController->home();
-            }
-        }
-        catch (Exception $e)
-        {
-            echo 'Erreur';
+        } catch (Exception $e) {
+            $this->errorController->errorServer();
         }
     }
 }
